dde3579e903374d6d8201aecab38711201a21b28
Paulo Rodrigues <prpaulo07@hotmail.com>
2021-06-17 09:46:20 -0300

    feat: structure of entities created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,33 @@ import { Module } from '@nestjs/common';
 import { StoreController } from './store/controllers/store.controller';
 import { StoreService } from './store/services/store.service';
 import { StoreModule } from './store/store.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-  imports: [StoreModule],
+  imports: [
+    StoreModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+    useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_DATABASE'),
+      entities: [__dirname + '/modules/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      synchronize: false,      
+      migrationsRun: false,
+      logging: true,
+    }),
+    inject: [ConfigService],
+  }),],
   controllers: [StoreController],
   providers: [StoreService],
 })
--- src/store/entities/address.entity.ts
+++ src/store/entities/address.entity.ts
@@ -1,9 +1,14 @@
-import { Column, Entity, OneToOne } from 'typeorm';
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 import { Store } from './store.entity';
 
 @Entity()
 export class Address {
+
+
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
   @Column({ length: 10 })
   country?: string;
 
