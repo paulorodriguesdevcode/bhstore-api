a4ae6f256f1a8d8ab028e4e9e15a1c3268128bca
Paulo Rodrigues <prpaulo07@hotmail.com>
2021-06-18 15:37:38 -0300

    fix: typeorm removed

--- src/modules/store/entities/address.entity.ts
+++ src/modules/store/entities/address.entity.ts
@@ -1,30 +1,20 @@
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 import { Store } from './store.entity';
 
 
 
-@Entity()
 export class Address {
 
-  @PrimaryGeneratedColumn('uuid')
   id?= uuid();
 
-  @Column({ length: 10 })
   country?: string;
 
-  @Column({ length: 100 })
   state?: string;
 
-  @Column({ length: 5 })
   city?: string;
 
-  @Column({ length: 100 })
   district?: string;
 
-  @OneToOne(() => Store, (store: Store) => store.address, {
-    onDelete: 'CASCADE',
-  })
   store?: Store;
 
   constructor(country:string, state:string, city:string, district:string) {
--- src/modules/store/entities/store.entity.ts
+++ src/modules/store/entities/store.entity.ts
@@ -1,26 +1,14 @@
-import { Column, Entity, JoinColumn,  OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import {Address} from './address.entity'
 import { v4 as uuid } from 'uuid';
 
 
-@Entity()
 export class Store  {
 
-  @PrimaryGeneratedColumn('uuid')
   id? = uuid();
 
-  @Column({ length: 100 })
   key?: string;
   
-  @OneToOne(() => Address, (address: Address) => address.store, {
-    nullable: true,
-    eager: true,
-    cascade: ['insert', 'remove', 'update'],
-    onDelete: 'CASCADE',
-  })
-  @JoinColumn()
   address?: Address;
-
 
   constructor(key:string, address:Address) {
     this.key = key;
